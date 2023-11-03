3ae969657daf920061b240c51f7994be14e2b7b4
Rejaul karim <76891535+Rejaul-arim-shohag@users.noreply.github.com>
2023-11-03 22:47:57 +0600

    project setup

---
+++ src/components/Menubar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Menubar: React.FC = () => {
+  return <div className="bg-blue-500 text-white p-4 !important">This is a component styled with Tailwind CSS!</div>;
+};
